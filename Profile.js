766815ba45a32a0634faa754335c3da30ad4d78f
Gaia Cicaloni <39329527+GaiaDCI@users.noreply.github.com>
2019-03-25 10:11:47 +0100

    Profile components
    user - profi and general structure

--- Profile.js
+++ Profile.js
@@ -1,37 +1,116 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-    Text,
-    View,
     Button,
-    StyleSheet,
-    Image
+    View,
+    Text,
+    Image,
+    StyleSheet
 } from 'react-native';
 
-import SignUpProfi from '../Signup/SignUpProfi'
-export default class Profile extends Component {
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
+import RequestFormular from '../RequestFormular';
+class ProfilePage extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <SignUpProfi />
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={styles.headerContent}>
+                    <Image
+                        style={styles.avatar}
+                        source={{ uri: 'https://cdn.dribbble.com/users/255/screenshots/2260728/avatar-colored-d.png' }}
+                    />
+                </View>
+                <View style={styles.bodyContentProfile}>
+                    <Text style={styles.username}>Company name</Text>
+                    <Text style={styles.shortDescription}>Short description Company</Text>
+                    <Text style={styles.longDescription}>service offer, service offer, service offer</Text>
+                    <Text style={styles.longDescription}>Description Company: Lorem ipsum dolor sit amet, voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text>
+                </View>
+                <Button
+                    title="Make a price request"
+                    onPress={() => this.props.navigation.navigate('Request')}
+                />
+            </View >
+        );
+    }
+}
+
+class RequestPage extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+                <RequestFormular />
+
+
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.navigate('ProfileScreen')}
+                />
             </View>
         );
     }
 }
 
+const RootStack = createStackNavigator(
+    {
+        ProfileScreen: {
+            screen: ProfilePage,
+        },
+        Request: {
+            screen: RequestPage,
+        },
+    },
+    {
+        initialRouteName: 'ProfileScreen',
+    }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+    render() {
+        return <AppContainer />;
+    }
+}
+
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF"
+    bodyProfile: {
+        alignSelf: 'center',
+        marginTop: 200,
+        alignItems: 'center',
+        flexDirection: 'row',
+        position: 'absolute',
+        backgroundColor: "#ffffff"
+    },
+    bodyContentProfile: {
+        margin: 10,
+        alignItems: 'center'
+    },
+    username: {
+        fontSize: 15,
+        fontWeight: '800',
+        margin: 10
+    },
+    shortDescription: {
+        fontSize: 13,
+        margin: 5
+    },
+    longDescription: {
+        fontSize: 11,
+        margin: 10,
+        padding: 10
+    },
+    inputs: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
     },
     avatar: {
-        width: 100,
-        height: 100,
-        marginBottom: 10,
+        width: 60,
+        height: 60,
         borderWidth: 3,
         borderRadius: 50,
         borderColor: 'black'
-    },
-});
+    }
+});  
